75ab5c9f7546b973442b0d1bf4899cfc04f62e6b
Bullseye777 <aurimux777@gmail.com>
2017-03-24 20:51:28 +0200

    php

--- Atvaizdavimas.php
+++ Atvaizdavimas.php
@@ -1,21 +1,47 @@
 <?php
+echo "<table style='border: solid 1px black;'>";
+echo "<tr>id<th>Id</th>pavadinimas<th>Pavadinimas</th>kaina<th>Kaina</th>Grupe</tr>";
+
+class TableRows extends RecursiveIteratorIterator
+{
+    function __construct($it)
+    {
+        parent::__construct($it, self::LEAVES_ONLY);
+    }
+
+    function current()
+    {
+        return "<td style='width:150px;border:1px solid black;'>" . parent::current() . "</td>";
+    }
+
+    function beginChildren()
+    {
+        echo "<tr>";
+    }
+
+    function endChildren()
+    {
+        echo "</tr>" . "\n";
+    }
+}
+
 $servername = "localhost";
 $username = "root";
 $password = "";
 
 try {
-    $dbh = new PDO('mysql:host=localhost;port=3306;dbname=meniu', $username, $password, array( PDO::ATTR_PERSISTENT => false));
+    $dbh = new PDO('mysql:host=localhost;port=3306;dbname=meniu', $username, $password, array(PDO::ATTR_PERSISTENT => false));
 
-    $stmt = $dbh->prepare("CALL getname()");
+    $stmt = $dbh->prepare("SELECT id, pavadinimas, kaina, grupe, diena FROM meniu");
 
     // call the stored procedure
     $stmt->execute();
 
     echo "<B>outputting...</B><BR>";
     while ($rs = $stmt->fetch(PDO::FETCH_OBJ)) {
-        echo "output: ".$rs->name."<BR>";
+        echo "output: " . $rs->name . "<BR>";
     }
-    echo "<BR><B>".date("r")."</B>";
+    echo "<BR><B>" . date("r") . "</B>";
 
 } catch (PDOException $e) {
     print "Error!: " . $e->getMessage() . "<br/>";

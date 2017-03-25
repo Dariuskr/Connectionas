ecbb23378d44a55ddda89e170341e6ff6dfee1d0
Bullseye777 <aurimux777@gmail.com>
2017-03-25 09:28:41 +0200

    php

--- Atvaizdavimas.php
+++ Atvaizdavimas.php
@@ -25,26 +25,3 @@ try {
     print "Error!: " . $e->getMessage() . "<br/>";
     die();
 }
-
-try {
-
-    $pavad = $_GET['pavadinimas'];
-    $pavad = $_GET['pavadinimas'];
-    $pavad = $_GET['pavadinimas'];
-    $pavad = $_GET['pavadinimas'];
-
-    $conn = new PDO('mysql:host=localhost;port=3306;dbname=meniu', $username, $password, array(PDO::ATTR_PERSISTENT => false)
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "INSERT INTO meniu (pavadinimas, kaina, grupe, diena)
-    VALUES (".$pavad.",".$kaina.", ".$grope.")";
-    // use exec() because no results are returned
-    $conn->exec($sql);
-    echo "New record created successfully";
-}
-catch(PDOException $e)
-{
-    echo $sql . "<br>" . $e->getMessage();
-}
-
-$conn = null;
--- insertas.php
+++ insertas.php
@@ -1,7 +1,27 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: A
- * Date: 2017.03.24
- * Time: 21:21
- */
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+try{
+
+    $pavad = $_GET['pavadinimas'];
+    $kaina = $_GET['kaina'];
+    $grupe = $_GET['grupe'];
+    $diena = $_GET['diena'];
+
+    $conn = new PDO('mysql:host=localhost;port=3306;dbname=meniu', $username, $password, array(PDO::ATTR_PERSISTENT => false));
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "INSERT INTO meniu (pavadinimas, kaina, grupe, diena)
+    VALUES (" . $pavad . "," . $kaina . "," . $grupe . "," . $diena . ")";
+    // use exec() because no results are returned
+    $conn->exec($sql);
+    echo "New record created successfully";
+} catch (PDOException $e) {
+
+    echo $sql . "<br>" . $e->getMessage();
+}
+
+$conn = null;

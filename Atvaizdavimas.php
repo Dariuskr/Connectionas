9d075fa29a3e2829d8162d4bda4823563a995b4d
teinted321 <darius.krei@gmail.com>
2017-03-27 12:11:41 +0300

    Delete funkcija su Get_Value

--- Atvaizdavimas.php
+++ Atvaizdavimas.php
@@ -63,9 +63,9 @@ try {
     $conn = new PDO("mysql:host=localhost;port=3306;dbname=meniu", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
+    $skaicius = $_GET[$skaicius];
     // sql to delete a record
-    $sql = "DELETE FROM meniu WHERE id=2";
+    $sql = "DELETE FROM meniu WHERE id=$skaicius";
     $preparedStatement = $conn->prepare($sql);
 
     // use exec() because no results are returned

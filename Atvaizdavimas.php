f201c2622f4d9387a467c631bfed86c53e0f123c
Bullseye777 <aurimux777@gmail.com>
2017-03-30 12:55:38 +0300

    Atvaizdavimas pagal savaites diena

--- Atvaizdavimas.php
+++ Atvaizdavimas.php
@@ -23,6 +23,82 @@ try {
     die();
 }
 
-
-
-
+//try {
+//    $dbh = new PDO('mysql:host=localhost;port=3306;dbname=meniu', $username, $password, array(PDO::ATTR_PERSISTENT => false));
+//
+//    $stmt = $dbh->prepare("SELECT * FROM `meniu`WHERE `diena` = 1");
+//
+//    // call the stored procedure
+//    $stmt->execute();
+//    $rs = $stmt->fetchAll(PDO::FETCH_OBJ);
+//
+//    echo json_encode($rs);
+//
+//} catch (PDOException $e) {
+//    print "Error!: " . $e->getMessage() . "<br/>";
+//    die();
+//}
+//
+//try {
+//    $dbh = new PDO('mysql:host=localhost;port=3306;dbname=meniu', $username, $password, array(PDO::ATTR_PERSISTENT => false));
+//
+//    $stmt = $dbh->prepare("SELECT * FROM `meniu`WHERE `diena` = 2");
+//
+//    // call the stored procedure
+//    $stmt->execute();
+//    $rs = $stmt->fetchAll(PDO::FETCH_OBJ);
+//
+//    echo json_encode($rs);
+//
+//} catch (PDOException $e) {
+//    print "Error!: " . $e->getMessage() . "<br/>";
+//    die();
+//}
+//
+//try {
+//    $dbh = new PDO('mysql:host=localhost;port=3306;dbname=meniu', $username, $password, array(PDO::ATTR_PERSISTENT => false));
+//
+//    $stmt = $dbh->prepare("SELECT * FROM `meniu`WHERE `diena` = 3");
+//
+//    // call the stored procedure
+//    $stmt->execute();
+//    $rs = $stmt->fetchAll(PDO::FETCH_OBJ);
+//
+//    echo json_encode($rs);
+//
+//} catch (PDOException $e) {
+//    print "Error!: " . $e->getMessage() . "<br/>";
+//    die();
+//}
+//
+//try {
+//    $dbh = new PDO('mysql:host=localhost;port=3306;dbname=meniu', $username, $password, array(PDO::ATTR_PERSISTENT => false));
+//
+//    $stmt = $dbh->prepare("SELECT * FROM `meniu`WHERE `diena` = 4");
+//
+//    // call the stored procedure
+//    $stmt->execute();
+//    $rs = $stmt->fetchAll(PDO::FETCH_OBJ);
+//
+//    echo json_encode($rs);
+//
+//} catch (PDOException $e) {
+//    print "Error!: " . $e->getMessage() . "<br/>";
+//    die();
+//}
+//
+//try {
+//    $dbh = new PDO('mysql:host=localhost;port=3306;dbname=meniu', $username, $password, array(PDO::ATTR_PERSISTENT => false));
+//
+//    $stmt = $dbh->prepare("SELECT * FROM `meniu`WHERE `diena` = 5");
+//
+//    // call the stored procedure
+//    $stmt->execute();
+//    $rs = $stmt->fetchAll(PDO::FETCH_OBJ);
+//
+//    echo json_encode($rs);
+//
+//} catch (PDOException $e) {
+//    print "Error!: " . $e->getMessage() . "<br/>";
+//    die();
+//}

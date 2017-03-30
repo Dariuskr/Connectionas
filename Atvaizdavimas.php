b04725425f7684827e369565e2cf70f1c4c82373
Bullseye777 <aurimux777@gmail.com>
2017-03-30 20:52:46 +0300

    Valio

--- Atvaizdavimas.php
+++ Atvaizdavimas.php
@@ -10,11 +10,39 @@ $password = "";
 
 
 try {
-    $dbh = new PDO('mysql:host=localhost;port=3306;dbname=meniu', $username, $password, array(PDO::ATTR_PERSISTENT => false));
 
+   $day =  $_GET['day'];
+
+    switch ($day){
+
+        case 'pirmadienis':
+            $diena = 1;
+            break;
+        case 'antradienis':
+            $diena = 2;
+            break;
+        case 'treciadienis':
+            $diena = 3;
+            break;
+        case 'ketvirtadienis':
+            $diena = 4;
+            break;
+        case 'penktadienis':
+            $diena = 5;
+            break;
+        default:
+            $diena = 1;
+            break;
+
+    }
+
+
+
+    $dbh = new PDO('mysql:host=localhost;port=3306;dbname=meniu', $username, $password, array(PDO::ATTR_PERSISTENT => false));
 
     $stmt = $dbh->prepare("SELECT * FROM `meniu` WHERE `diena` = ?");
 
+    $stmt->bindParam(1, $diena);
 
 
     // call the stored procedure
